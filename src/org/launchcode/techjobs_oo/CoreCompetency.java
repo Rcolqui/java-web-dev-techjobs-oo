5058310bb1af541ea017cb2df51cfaf41f4eace9
Robin <robincolquitt@yahoo.com>
2021-02-12 16:08:06 -0600

    Completed step two

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -12,6 +12,7 @@ public class CoreCompetency {
         nextId++;
     }
 
+
     public CoreCompetency(String value) {
         this();
         this.value = value;
@@ -37,6 +38,20 @@ public class CoreCompetency {
         return Objects.hash(id);
     }
 
+
+
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
